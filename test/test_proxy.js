2f7fcdc3c656c9405c375cf739aac4556e76b7fb
jackie lee <catiga03@gmail.com>
2024-04-03 20:46:24 +0800

    add test code

---
+++ test/test_proxy.js
@@ -0,0 +1,34 @@
+const NovaRouter = artifacts.require('NovaRouter');
+const NovaProxy = artifacts.require('NovaProxy');
+
+contract('NovaRouter through NovaProxy', (accounts) => {
+    describe("cleanState", function() {
+        let novaRouterInstance;
+        let novaProxyInstance;
+
+        it(`should return Name:`, async () => {
+            novaRouterInstance = await NovaRouter.at('0x398257729721c4070459496ba1cACD254ceA3CEc')
+            novaProxyInstance = await NovaProxy.at('0x765dE71f18b444C4EAd36786b22b71E19BC71D58')
+            const owner = await novaProxyInstance.owner();
+            const impl = await novaProxyInstance.getImplementation()
+            const novaLogicInstanceAtProxyAddress = new web3.eth.Contract(NovaRouter.abi, novaProxyInstance.address);
+
+            // await novaLogicInstanceAtProxyAddress.methods.initialize().send({ from: accounts[0], gasPrice:8000000000, gas:5000000 });
+            const selltax = await novaLogicInstanceAtProxyAddress.methods.sellTax().call()
+            console.log('owner', owner)
+            console.log('impl', impl)
+            console.log('sell tax', selltax)
+            console.log('accounts', accounts)
+
+            const tokenLength = await novaLogicInstanceAtProxyAddress.methods.tokenLength().call()
+            console.log('tokenLength', tokenLength)
+
+            // const result = await novaLogicInstanceAtProxyAddress.methods.adjustTax(20, 20).send({ from: accounts[0], gasPrice:8000000000, gas:5000000 });
+            // console.log('adjust tax result', result)
+            
+            // const result = await novaLogicInstanceAtProxyAddress.methods.reinitialize(500000).send({ from: accounts[0], gasPrice:8000000000, gas:5000000 });
+            // console.log('adjust reinitialize result', result)
+        });
+    })
+  
+});
